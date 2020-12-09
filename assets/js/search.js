d1c1612346a8e9801badabff6b182628c45a7735
aungpaing98 <aungpaing98@users.noreply.github.com>
2020-12-09 09:10:18 +0000

    deploy: dd10aecd1db0291c378b88979241be65ca6fe224

--- assets/js/search.js
+++ assets/js/search.js
@@ -47,7 +47,7 @@ jtd.onReady = function(ready) {
 
 function initSearch() {
     var request = new XMLHttpRequest();
-    request.open('GET', '/Resource_Blog/assets/js/search-data.json', true);
+    request.open('GET', '/Resource-Blog/assets/js/search-data.json', true);
   
     request.onload = function(){
       if (request.status >= 200 && request.status < 400) {
